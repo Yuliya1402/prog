86ea372b6f0cd22723518e4dc01b6bc1aa8895f7
Yuliya Turmacheva <Yuliyat1402@gmail.com>
2023-12-25 01:08:19 +0300

    Создали переменные с разными типами данных

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Console.WriteLine("Hello, World!");
+
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55;
+
+bool b = true;
+Console.WriteLine(d);
+
+Console.WriteLine(b);
