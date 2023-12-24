eed310b44caba2707d435fa91032c09a03bea8ba
Yuliya Turmacheva <Yuliyat1402@gmail.com>
2023-12-25 01:59:03 +0300

    Задача про 5 гирь

---
+++ task4/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 6;
+int b = 7;
+int c = 8;
+int d = 2;
+int e = 10;
+
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
